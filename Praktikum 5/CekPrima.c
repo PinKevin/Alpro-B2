597ebbe49e1c5aeeb70c1c9b9a847aceba16b56e
Emerio Kevin <emeriokevin2320@gmail.com>
2022-03-24 14:41:16 +0700

    Add files via upload

---
+++ Praktikum 5/CekPrima.c
@@ -0,0 +1,47 @@
+/*
+Nama file : CekPrima.c
+Deskripsi : program ini digunakan untuk mengecek apakah
+            sebuah bilangan integer positif prima atau tidak
+Pembuat : Emerio Kevin Aryaputra - 24060121120012
+Waktu dibuat : Senin, 21 Maret 2022 - 21.42 WIB
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // kamus
+    int N, i, cek;
+
+    // algoritma
+    printf("Masukkan bilangan = ");
+    scanf("%d", &N);
+
+    cek = 1;
+
+    if (N <= 0) {
+        printf("\nBilangan harus positif\n");
+    } else if (N == 1) {
+        printf("\nBukan bilangan prima\n", N);
+    } else {
+        for(i = 2; i <= sqrt(N); i++){
+            if(!(N % i)){
+                cek = 0;
+            }
+        }
+
+        if (cek) {
+          printf("\nBilangan prima\n", N);
+        } else {
+            printf("\nBukan bilangan prima\n", N);
+        }
+    }
+
+
+
+    return 0;
+}
+
+
+
+
---
+++ Praktikum 5/CekSempurna.c
@@ -0,0 +1,47 @@
+/*
+Nama file : CekSempurna.c
+Deskripsi : program ini digunakan untuk mengecek apakah
+            sebuah bilangan integer sempurna atau tidak,
+            yaiu faktornya tanpa dirinya sendiri apabila
+            dijumlah menghasilkan bilangan itu sendiri
+Pembuat : Emerio Kevin Aryaputra - 24060121120012
+Waktu dibuat : Senin, 21 Maret 2022 - 21.53 WIB
+*/
+
+#include <stdio.h>
+
+int main() {
+    // kamus
+    int N, i, perf;
+
+    // algoritma
+    printf("Masukkan bilangan = ");
+    scanf("%d", &N);
+
+    perf = 0;
+
+    if (N <= 0) {
+        printf("\nBilangan harus positif\n");
+    } else {
+        for (i = 1; i < N; i++){
+            if (N % i == 0) {
+                perf += i;
+            }
+        }
+
+        if (N == perf) {
+          printf("\nBilangan sempurna\n", N);
+        } else {
+            printf("\nBukan bilangan sempurna\n", N);
+        }
+    }
+
+
+
+    return 0;
+}
+
+
+
+
+
